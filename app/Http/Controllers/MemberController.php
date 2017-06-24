f104c2c32e612f8f3f127aaf0077b7cd98152ef8
Chindanai Wasanachin <dn.chindanai@gmail.com>
2017-06-25 00:41:29 +0700

    return data when response json

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Member;
 
 class MemberController extends Controller
 {
@@ -29,18 +30,37 @@ class MemberController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
         //
+        $member = new Member();
+        $memberDetail = $request->toArray();
+        $memberDetail['password'] = hash('md5', $memberDetail['password']);
+        $member->fill($memberDetail);
+        if ($member->save()) {
+            $data = [
+                "result" => "Success",
+                "data" => null,
+                "errorMessage" => null
+            ];
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                "result" => "Fail",
+                "data" => null,
+                "errorMessage" => null
+            ];
+            return response()->json($data, 400);
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -51,7 +71,7 @@ class MemberController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -62,8 +82,8 @@ class MemberController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -74,7 +94,7 @@ class MemberController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
