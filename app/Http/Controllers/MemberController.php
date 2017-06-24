e8ab9b62f557af7c71737f15a51f19e44d94aca2
Chindanai Wasanachin <dn.chindanai@gmail.com>
2017-06-25 00:33:41 +0700

    add fillable model

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -14,7 +14,6 @@ class MemberController extends Controller
     public function index()
     {
         //
-        echo "helloWorld";
     }
 
     /**
--- app/Member.php
+++ app/Member.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Member extends Model
 {
     //
+    protected $table = "member";
+
+    protected $fillable = ['first_name', 'last_name', 'username', 'password', 'birth_date', 'email', 'mobile_no', 'title'];
+
 }
